7382ca4ca2e7c60cec07f11fda8e73b47e1d10b7
yashasm19 <150432620+yashasm19@users.noreply.github.com>
2026-05-20 23:40:59 +0530

    Update student_app.py

--- student_app.py
+++ student_app.py
@@ -2,28 +2,71 @@ students = []
 
 while True:
     print("\n--- Student Management System ---")
-    print("1. Add new Student")
-    print("2. Add branch")
-    print("3. exit")
-    
+    print("1. Add Student")
+    print("2. Add Branch")
+    print("3. Add Phone Number")
+    print("4. View Students")
+    print("5. Exit")
 
     choice = input("Enter choice: ")
 
     if choice == "1":
         name = input("Enter student name: ")
-        students.append(name)
+
+        student = {
+            "name": name,
+            "branch": "",
+            "phone": ""
+        }
+
+        students.append(student)
+
         print(f"{name} added successfully!")
 
     elif choice == "2":
-        print("\nadd branch:")
-        
-        if len(branch) == 0:
-            print("others")
-        else:
-            for branch in students:
-                print(student)
+
+        student_name = input("Enter student name to add branch: ")
+
+        for student in students:
+
+            if student["name"] == student_name:
+
+                branch = input("Enter branch: ")
+                student["branch"] = branch
+
+                print("Branch updated successfully!")
 
     elif choice == "3":
+
+        student_name = input("Enter student name to add phone number: ")
+
+        for student in students:
+
+            if student["name"] == student_name:
+
+                phone = input("Enter phone number: ")
+                student["phone"] = phone
+
+                print("Phone number updated successfully!")
+
+    elif choice == "4":
+
+        print("\n--- Student Details ---")
+
+        if len(students) == 0:
+            print("No students found")
+
+        else:
+
+            for student in students:
+
+                print(f"""
+Name   : {student['name']}
+Branch : {student['branch']}
+Phone  : {student['phone']}
+                """)
+
+    elif choice == "5":
         print("Exiting program...")
         break
 
