dbb4d0d9f1743a2e22346e972e6596edcd61e2f3
yashas <yashasmgowda2004@gmail.com>
2026-05-22 14:43:12 +0530

    updated student app management

--- student_app.py
+++ student_app.py
@@ -1,6 +1,7 @@
 students = []
 
 while True:
+
     print("\n--- Student Management System ---")
     print("1. Add Student")
     print("2. Add Branch")
@@ -10,7 +11,9 @@ while True:
 
     choice = input("Enter choice: ")
 
+    # ADD STUDENT
     if choice == "1":
+
         name = input("Enter student name: ")
 
         student = {
@@ -23,52 +26,78 @@ while True:
 
         print(f"{name} added successfully!")
 
+    # ADD BRANCH
     elif choice == "2":
 
         student_name = input("Enter student name to add branch: ")
+
+        found = False
 
         for student in students:
 
             if student["name"] == student_name:
 
                 branch = input("Enter branch: ")
+
                 student["branch"] = branch
 
                 print("Branch updated successfully!")
 
+                found = True
+
+        if found == False:
+            print("Student not found")
+
+    # ADD PHONE NUMBER
     elif choice == "3":
 
         student_name = input("Enter student name to add phone number: ")
+
+        found = False
 
         for student in students:
 
             if student["name"] == student_name:
 
                 phone = input("Enter phone number: ")
+
                 student["phone"] = phone
 
                 print("Phone number updated successfully!")
 
+                found = True
+
+        if found == False:
+            print("Student not found")
+
+    # VIEW STUDENTS
     elif choice == "4":
 
         print("\n--- Student Details ---")
 
         if len(students) == 0:
+
             print("No students found")
 
         else:
 
             for student in students:
 
-                print(f"""
-Name   : {student['name']}
-Branch : {student['branch']}
-Phone  : {student['phone']}
-                """)
+                print(f"\nName : {student['name']}")
 
+                if student['branch'] != "":
+                    print(f"Branch : {student['branch']}")
+
+                if student['phone'] != "":
+                    print(f"Phone : {student['phone']}")
+
+    # EXIT
     elif choice == "5":
+
         print("Exiting program...")
         break
 
+    # INVALID OPTION
     else:
+
         print("Invalid choice")
